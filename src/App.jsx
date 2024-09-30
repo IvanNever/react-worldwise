bab0fc3dbcf2dcf8b49c95d43b272a67b06fd04d
Ivan Neverovsky <ivan@neverovsky.com.ua>
2024-09-30 18:43:44 +0300

    added main pages and base navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product.jsx";
+import Pricing from "./pages/Pricing.jsx";
+import Homepage from "./pages/Homepage.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
+import PageNav from "./components/PageNav.jsx";
+
 export default function App() {
-  return <div>Worldwise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,10 @@
+import PageNav from "../components/PageNav.jsx";
+
+export default function PageNotFound() {
+  return (
+    <div>
+      <PageNav />
+      <p>Page not found</p>
+    </div>
+  );
+}
