dcf81003c8164262eaf05f3ed6f17420384d1643
Ivan Neverovsky <ivan@neverovsky.com.ua>
2024-10-01 12:08:50 +0300

    created app layout

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,4 +1,16 @@
 import style from "./AppNav.module.css";
+import { NavLink } from "react-router-dom";
 export default function AppNav() {
-  return <div className={style.nav}>App navigation</div>;
+  return (
+    <nav className={style.nav}>
+      <ul>
+        <li>
+          <NavLink to="cities">Cities</NavLink>
+        </li>
+        <li>
+          <NavLink to="countries">Countries</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,9 +1,11 @@
-import AppNav from "../components/AppNav.jsx";
-
+import Sidebar from "../components/Sidebar.jsx";
+import Map from "../components/Map.jsx";
+import styles from "./AppLayout.module.css";
 export default function AppLayout() {
   return (
-    <div>
-      <AppNav />
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
     </div>
   );
 }
